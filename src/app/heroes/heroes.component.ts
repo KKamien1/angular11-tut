62ca1edacec9adbef8b0ef44179030f7dc3d7154
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2020-12-22 16:23:51 +0100

    Adds Messages Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import {Hero} from '../hero'
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +10,17 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    name: 'Windstorm',
-    id: 1
-  }
+
+  title: string = 'Hero';
   heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
   
   ngOnInit(): void {
