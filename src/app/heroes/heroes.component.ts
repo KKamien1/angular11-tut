8091d696a012160da1eb10863f76445a47db1d66
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2020-12-22 16:06:58 +0100

    Adds Hero.Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,17 +12,23 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm',
     id: 1
   }
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { 
+  constructor(private heroService: HeroService) {
+  }
+  
+  ngOnInit(): void {
+    this.getHeroes()
   }
 
-  ngOnInit(): void {
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
