e63247c484ce4dd007524893d1b4328cdda2c077
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2020-12-23 15:27:14 +0100

    Add in-app navigation with routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import {Hero} from '../hero'
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,27 +9,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  title: string = 'Hero';
   heroes: Hero[];
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {
-  }
-  
-  ngOnInit(): void {
-    this.getHeroes()
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
-
